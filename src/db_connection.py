95dac10cc585769ff58be5bb877d9e135d7587d5
“alejandra-gutierrez” <ag1917@ic.ac.uk>
2021-10-04 20:12:10 +0200

    with heroku database connection

--- src/db_connection.py
+++ src/db_connection.py
@@ -10,6 +10,7 @@ import dash
 
 engine = create_engine('postgresql://iyoalfhnzaqaqy:54e38dd1ecf2b4a25c9158b51c3ed0efbc8a8336c5c721f9ba5d7af65b7b6315@ec2-54-170-163-224.eu-west-1.compute.amazonaws.com:5432/d7ssmu85htau94')
 
+
 Session=sessionmaker(bind=engine)
 session=Session()
 
